e800f90de5abc6448a0c7a75f361560b7fa18ba5
Joanez Andrades <suportedeathofday@gmail.com>
2021-02-08 16:48:28 -0300

    Adicionar abas Testes, Perguntas e Vouchers no painel do plugin

---
+++ admin/partials/templates/tpl-list-questions.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Provide a table with all questions and answers
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       unitycode.tech
+ * @since      1.0.0
+ *
+ * @package    Uc_Qpt
+ * @subpackage Uc_Qpt/admin/partials/templates/
+ */
+
+$ajax_url = admin_url('admin-ajax.php');
+$args = array(
+    'post_type'         => 'uc_question',
+    'post_status'       => 'publish',
+    'posts_per_page'    => -1,
+);
+$query = new WP_Query($args);
+
+# Testando resultados registrados no voucher
+$v_is_used = get_post_meta( 920, 'ucqpt_is_used', true );
+$v_result_test_data = get_post_meta( 920, 'ucqpt_result_test_data', true );
+
+?>
+
+<?php 
+if ( $query->have_posts() ) :
+    ?>
+    <table class="uk-table uk-table-striped">
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Perguntas</th>
+                <th>Resultados</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        while ( $query->have_posts() ) :
+            $query->the_post();
+            $post_id            = get_the_ID();
+            $questions_str      = get_post_meta( $post_id, 'quiz_questions_ids', true );
+            $questions_arr      = explode(',', $questions_str);
+            $questions_total    = count($questions_arr);
+
+            ?>
+            <tr>
+                <td><?php the_title(); ?></td>
+                <td><?php echo $questions_total; ?></td>
+                <td>0</td>
+            </tr>
+            <?php
+        endwhile;
+        ?>
+        </tbody>
+    </table>
+    <?php
+endif;
+?>
--- admin/partials/uc-qpt-admin-display.php
+++ admin/partials/uc-qpt-admin-display.php
@@ -12,21 +12,42 @@
  * @subpackage Uc_Qpt/admin/partials
  */
 ?>
-<nav class="uk-navbar-container" uk-navbar>
+<div class="uk-container">
 
-    <div class="uk-navbar-left">
-    <h1 class="uk-heading-bullet">Bem Vindo(a) - </h1>
-        <ul class="uk-navbar-nav uk-flex uk-flex-middle">
-            <li class="">
-                <a href="#"><button class="uk-button uk-button-primary uk-button-large" uk-toggle="target: #new-quiz">Adicionar Teste de Personalidade</button></a>
-            </li>
-            <li class="">
-                <a href="#"><button class="uk-button uk-button-primary uk-button-large" uk-toggle="target: #register-company">Adicionar Empresa</button></a>
-            </li>
-            <li class="">
-                <a href="#"><button class="uk-button uk-button-primary uk-button-large" uk-toggle="target: #register-voucher">Adicionar Voucher</button></a>
-            </li>
-        </ul>
+    <nav class="uk-navbar-container" uk-navbar>
 
-    </div>
-</nav>
+        <div class="uk-navbar-left">
+        <h1 class="uk-heading-bullet">Bem Vindo(a) - </h1>
+            <ul class="uk-navbar-nav uk-flex uk-flex-middle">
+                <li class="">
+                    <a href="#"><button class="uk-button uk-button-primary uk-button-large" uk-toggle="target: #new-quiz">Adicionar Teste de Personalidade</button></a>
+                </li>
+                <li class="">
+                    <a href="#"><button class="uk-button uk-button-primary uk-button-large" uk-toggle="target: #register-company">Adicionar Empresa</button></a>
+                </li>
+                <li class="">
+                    <a href="#"><button class="uk-button uk-button-primary uk-button-large" uk-toggle="target: #register-voucher">Adicionar Voucher</button></a>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+
+    <ul class="uk-subnav uk-subnav-pill" uk-switcher>
+        <li><a href="#">Testes</a></li>
+        <li><a href="#">Perguntas/Respostas</a></li>
+        <li><a href="#">Vouchers</a></li>
+    </ul>
+    <ul class="uk-switcher uk-margin">
+        <li><?php require_once plugin_dir_path( __FILE__ ) . 'templates/tpl-list-all-quizes.php'; ?></li>
+        <li><?php require_once plugin_dir_path( __FILE__ ) . 'templates/tpl-list-questions.php'; ?></li>
+        <li><?php require_once plugin_dir_path( __FILE__ ) . 'templates/tpl-list-vouchers.php'; ?></li>
+    </ul>
+
+    <?php    
+    require_once plugin_dir_path( __FILE__ ) . 'uc-qpt-new-quiz.php';
+    require_once plugin_dir_path( __FILE__ ) . 'templates/tpl-register-company.php';
+    require_once plugin_dir_path( __FILE__ ) . 'templates/tpl-create-voucher.php';
+    ?>
+
+</div>
