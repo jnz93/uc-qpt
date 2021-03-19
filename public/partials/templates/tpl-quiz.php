4cdb7be3068bd6246eed8c126bf2f7efb7e44534
Joanez Andrades <suportedeathofday@gmail.com>
2021-03-19 17:22:20 -0300

    Fix: apresentar perguntas no formato step-by-step

--- public/partials/templates/tpl-quiz.php
+++ public/partials/templates/tpl-quiz.php
@@ -31,6 +31,9 @@ $ajax_url 	= admin_url( 'admin-ajax.php' );
             </ul>
             <span class="">OBS: Use a pontuação 1, 2, 4 e 6 em todas as questões, na ordem que escolher, sem repetir valores.</span>
         </div>
+
+        <!-- # Perguntas -->
+        <div id="regForm" class="">
         <?php 
         foreach ( $idsarr as $id ) :
 
@@ -47,43 +50,62 @@ $ajax_url 	= admin_url( 'admin-ajax.php' );
                     'posts_per_page'=> -1,
                 );
                 $answers = get_posts( $args );
+                ?>
+                <div class="wrapper-question tab" data-id="<?php echo $id; ?>">
+                    <h4 id="" class=""><?php echo $number .' - '. $title_question; ?></h4>
+                    <?php
+                    if ( !empty($answers) ) :
+                        $letters = array('a)', 'b)', 'c)', 'd)', 'e)', 'f)');
+                        $pos = 0; ?>
 
-                # Front-end
-                $question .= '<div class="wrapper-question" data-id="'. $id .'"><h4 id=" class="">'. $number .' - '. $title_question .'</h4>';
-                
-                if ( !empty($answers) ) :
-                    $letters = array('a)', 'b)', 'c)', 'd)', 'e)', 'f)');
-                    $pos = 0;
-                    $options = '<div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">';
-                    foreach ( $answers as $answer ) :
-                        $options .= '<form class="uk-width-1-1" uk-grid>
-                                        <label class="uk-width-1-1@s">
-                                            <input class="uk-radio" type="checkbox" name="group-'. $id .'" data-id="'. $answer->ID .'"> '. ucfirst($letters[$pos]).' '. $answer->post_title .'
-                                            <input class="uk-input uk-form-width-small" type="number" id="" placeholder="Peso" style="display: none;">
-                                            <select name="uk-input uk-form-width-small" id="" class="" onChange="filterWeights(jQuery(this))">
-                                                <option value="0" class="" selected>Selecione o peso</option>
-                                                <option value="1" class="">1</option>
-                                                <option value="2" class="">2</option>
-                                                <option value="4" class="">4</option>
-                                                <option value="6" class="">6</option>
-                                            </select>
-                                        </label>';
-                        $pos++;
-                    endforeach;
-                    $options .= '<button type="reset" class="uk-button uk-button-default" onclick="resetAnswer(jQuery(this))">Resetar pergunta</button></form>';
-                endif;
-
-                $question .= $options;
-                $question .= '</div>';
-                $number++;
+                        <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">                        
+                        <?php
+                        foreach ( $answers as $answer ) : ?>
+                            <form class="uk-width-1-1" uk-grid>
+                                    <label class="uk-width-1-1@s">
+                                        <input class="uk-radio" type="checkbox" name="<?php echo 'group-'. $id;?>" data-id="<?php echo $answer->ID ?>"> <?php echo ucfirst($letters[$pos]).' '. $answer->post_title ?>
+                                        <input class="uk-input uk-form-width-small" type="number" id="" placeholder="Peso" style="display: none;">
+                                        <select name="" id="" class="uk-select uk-form-width-small" onChange="filterWeights(jQuery(this))" oninput="this.className = ''">
+                                            <option value="0" class="" selected>Selecione o peso</option>
+                                            <option value="1" class="">1</option>
+                                            <option value="2" class="">2</option>
+                                            <option value="4" class="">4</option>
+                                            <option value="6" class="">6</option>
+                                        </select>
+                                    </label> 
+                                    <?php
+                                $pos++;
+                        endforeach; ?>
+                                <button type="reset" class="uk-button uk-button-default" onclick="resetAnswer(jQuery(this))">Resetar pergunta</button>
+                            </form>
+                        </div>
+                        <?php
+                    endif;
+                    $number++;
+                    ?>
+                </div>
+                <?php
             endif;
         endforeach;
-        echo $quizContent;
-        echo $caution;
-        echo $question;
         ?>
+
+        <!-- Control Buttons -->
+        <div style="overflow:auto;">
+            <div style="float:right;">
+                <button type="button" id="prevBtn" onclick="nextPrev(-1)">Anterior</button>
+                <button type="button" id="nextBtn" onclick="nextPrev(1)">Próxima</button>
+            </div>
+        </div>
+
+        <!-- Circles which indicates the steps of the form: -->
+        <div style="text-align:center;margin-top:40px;">
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+        </div>
     </div>
-<div>
-    <button class="btn-primary" type="button" onclick="submitAnswers(jQuery('.wrapper-question'), '<?php echo $quiz_id; ?>', '<?php echo $voucher_id ?>', '<?php echo $ajax_url; ?>')">Responder Quiz</button>
-</div>
+    <div>
+        <button class="btn-primary" type="button" onclick="submitAnswers(jQuery('.wrapper-question'), '<?php echo $quiz_id; ?>', '<?php echo $voucher_id ?>', '<?php echo $ajax_url; ?>')">Responder Quiz</button>
+    </div>
 </div>
