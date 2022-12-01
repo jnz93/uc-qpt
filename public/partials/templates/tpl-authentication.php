e37c0bb6b006249ffe2b70c098b5bbbceeda54ee
Joanez Andrades <suportedeathofday@gmail.com>
2022-12-01 15:33:07 -0300

    Correções no layout e design dos formulários de validação e autenticação

--- public/partials/templates/tpl-authentication.php
+++ public/partials/templates/tpl-authentication.php
@@ -1,33 +1,38 @@
-<div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-center">
-    <div class="form-authentication">
-        <p class="uk-text-bolder"><?php _e( 'Complete suas informações antes de iniciar a avaliação.', 'textdomain' ); ?></p>
-    
-        <form class="uk-form">
-            <div class="uk-margin">
-                <label class="uk-form-label" for="user_full_name"><?php _e( 'Nome completo', 'textdomain' ); ?></label>
-                <div class="uk-form-controls">
-                    <input class="uk-input uk-text-center uk-text-normal uk-text-emphasis" id="user_full_name" type="text" placeholder="Nome completo">
-                </div>
+
+<div class="uk-width-1-1 uk-padding uk-text-center">
+    <h3 class="uk-text-bold"><?php _e( 'Autenticação', 'textdomain') ?></h3>
+    <p class="uk-text-bolder"><?php _e( 'Complete as informações antes de iniciar a avaliação.', 'textdomain' ); ?></p>
+
+    <form class="uk-form uk-margin-medium-top">
+        <div class="uk-margin">
+            <label class="uk-form-label" for="user_full_name"><?php _e( 'Nome completo', 'textdomain' ); ?></label>
+            <div class="uk-form-controls">
+                <input class="uk-input uk-text-center uk-text-normal uk-text-emphasis formValidation__input" id="user_full_name" type="text" placeholder="Nome completo">
             </div>
-    
-            <div class="uk-margin">
-                <label class="uk-form-label" for="user_email"><?php _e( 'E-mail', 'textdomain') ?></label>
-                <div class="uk-form-controls">
-                    <input class="uk-input uk-text-center uk-text-normal uk-text-emphasis" id="user_email" type="text" placeholder="E-mail">
-                </div>
+        </div>
+
+        <div class="uk-margin">
+            <label class="uk-form-label" for="user_email"><?php _e( 'E-mail', 'textdomain') ?></label>
+            <div class="uk-form-controls">
+                <input class="uk-input uk-text-center uk-text-normal uk-text-emphasis formValidation__input" id="user_email" type="text" placeholder="E-mail">
             </div>
-            
-            <div class="uk-margin">
-                <label class="uk-form-label" for="user_phone"><?php _e( 'Telefone', 'textdomain' ); ?></label>
-                <div class="uk-form-controls">
-                    <input class="uk-input uk-text-center uk-text-normal uk-text-emphasis" id="user_phone" type="text" placeholder="(ddd) xxxxx-xxxx">
-                </div>
+        </div>
+        
+        <div class="uk-margin">
+            <label class="uk-form-label" for="user_phone"><?php _e( 'Telefone', 'textdomain' ); ?></label>
+            <div class="uk-form-controls">
+                <input class="uk-input uk-text-center uk-text-normal uk-text-emphasis formValidation__input" id="user_phone" type="text" placeholder="(ddd) xxxxx-xxxx">
             </div>
-            
-            <div class="uk-margin">
-                <button type="button" class="uk-button uk-button-primary" onclick="authenticateVoucher()"><?php _e( 'Autenticar Voucher', 'textdomain'); ?></button>
-            </div>
-            <input type="hidden" name="voucher_id" id="voucher_id" value="<?php echo $vId; ?>">
-        </form>
-    </div>
+        </div>
+        
+        <div class="uk-margin">
+            <button type="button" class="uk-button uk-box-shadow-small uk-box-shadow-hover-medium formValidation__submit" onclick="authenticateVoucher()"><?php _e( 'Autenticar Voucher', 'textdomain'); ?></button>
+        </div>
+        <input type="hidden" name="voucher_id" id="voucher_id" value="<?php echo $vId; ?>">
+    </form>
 </div>
+<script>
+    jQuery(document).ready(function(){
+        jQuery('#user_phone').mask('(00) 0 0000-0000');
+    });
+</script>
--- public/partials/templates/tpl-welcome.php
+++ public/partials/templates/tpl-welcome.php
@@ -2,31 +2,33 @@
     <div class="uk-modal-dialog" style="background-color: #fdfcfc !important;">
         <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
         <div id="modal-content" class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-            <div class="uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-animation-slide-top" data-src="https://metodoprav.com.br/wp-content/uploads/2022/09/pexels-andrew-neel-3178818.jpg" uk-img uk-height-viewport></div>
-            <div class="uk-padding-large uk-animation-slide-bottom">
+            <!-- COVER -->
+            <div class="uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-animation-slide-top" data-src="https://metodoprav.com.br/wp-content/uploads/2022/09/pexels-andrew-neel-3178818.jpg" uk-img uk-height-viewport>
                 <div class="modal-header uk-text-center">
-                    <img class="logotipo" src="https://metodoprav.com.br/wp-content/uploads/2021/04/logotipo-PRAV-e1617632526222.jpg" alt="">
-                    <h3 class="uk-text-bold"><?php _e( 'Inventário de Personalidade', 'textdomain' ); ?></h3>
+                    <img class="welcome__logotipo" src="https://metodoprav.com.br/wp-content/uploads/2022/11/logotipo-PRAV-e1617632526222-removebg-preview.png" alt="">
+                    <h3 class="welcome__title uk-text-bold"><?php _e( 'Inventário de Personalidade', 'textdomain' ); ?></h3>
                 </div>
-                <div id="form-wrapper" class="">
-                    <div class="uk-card uk-card-default uk-card-hover uk-card-body uk-text-center">
-                        <div class="form-validate">
-                            <p class="uk-text-bolder"><?php _e( 'Seja bem vindo(a)! Faça a validação do voucher para continuar.', 'textdomain' ); ?></p>
-                            <div class="uk-margin">
-                                <label class="uk-form-label" for="ucqpt_voucher_code"><?php _e( 'Voucher', 'textdomain' ); ?></label>
-                                <div class="uk-form-controls">
-                                    <input class="uk-input uk-input uk-form-width-medium uk-form-large uk-text-center uk-text-uppercase uk-text-bold uk-text-emphasis" id="ucqpt_voucher_code" type="text" placeholder="Ex: XXXXXXXX-XXX">
-                                </div>
+                <div class="welcome__coverBackground"></div>
+            </div>
+
+            <!-- FORM -->
+            <div class="uk-padding-large uk-animation-slide-bottom">
+
+                <div id="form-wrapper" class="uk-flex uk-flex-center uk-flex-middle uk-box-shadow-medium formValidation">
+                    <div class="uk-width-1-1 uk-padding uk-text-center">
+                        <h3 class="uk-text-bold"><?php _e( 'Bem vindo(a)!', 'textodmain'); ?></h3>
+                        <p class=""><?php _e( 'Faça a validação do voucher para prosseguir.', 'textdomain' ); ?></p>
+                        <form class="form-validate uk-margin-medium-top">
+                            <label class="uk-form-label formValidation__label" for="ucqpt_voucher_code"><?php _e( 'Insira o voucher', 'textdomain' ); ?></label>
+                            <div class="uk-form-controls">
+                                <input class="uk-input uk-text-center uk-text-uppercase uk-text-bold uk-text-emphasis formValidation__input" id="ucqpt_voucher_code" type="text" placeholder="Ex: XXXXXXXX-XXX">
                             </div>
-        
-                            <div class="uk-margin">
-                                <button type="button" id="validateVoucher" class="uk-button uk-button-primary"><?php _e( 'Validar voucher', 'textdomain' ); ?></button>
-                            </div>
-                        </div>
+                            <button type="button" id="validateVoucher" class="uk-button uk-box-shadow-small uk-box-shadow-hover-medium formValidation__submit"><?php _e( 'Solicitar Validação', 'textdomain' ); ?></button>
+                        </form>
                     </div>
                 </div>
+                <input type="hidden" name="quiz_id" id="quiz_id" value="<?php echo $quizId; ?>">
             </div>
         </div>
     </div>
-    <input type="hidden" name="quiz_id" id="quiz_id" value="<?php echo $quizId; ?>">
 </div>
